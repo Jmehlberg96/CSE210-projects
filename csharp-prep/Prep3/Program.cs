b2d38718c6c6d6bad90e160b6aed747b6150ab6b
jmehlberg96 <meh22002@byui.edu>
2023-01-09 14:34:18 -0700

    program 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        //Console.Write("What is the magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("I have a magic number. Can you guess what it is? ");
+      
+        int guess = -1;
+  
+        {
+            while (guess != number)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+               
+            }
+        
+        }
+    }    
 }
