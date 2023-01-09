e3a684e7d701115f9b0b953a70e9a47f8c780067
jmehlberg96 <meh22002@byui.edu>
2023-01-09 16:02:51 -0700

    program 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int>numbers = new List<int>();
+
+       int userNumber = -1;
+       int runningTotal = 0;
+       int largestNum = -1;
+
+       Console.WriteLine("Enter a list of numbers. Type 0 when finished.");
+
+        //add numbers to a list
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number. ");
+            string userInput = Console.ReadLine();
+            userNumber = int.Parse(userInput);
+
+            if (userNumber != 0)
+
+                numbers.Add(userNumber);
+        }
+
+        //compute the running total of the list
+        foreach (int number in numbers)
+        {
+            runningTotal += number;
+        }
+        Console.WriteLine($"Your sum is {runningTotal}.");
+
+        //compute the average of the list
+        int count = numbers.Count;
+        int average = runningTotal / count;
+
+        Console.WriteLine($"The average of your numbers is {average}.");
+
+        foreach (int number in numbers)
+        {
+            if (number > largestNum)
+            {
+                largestNum = number;
+            }
+        }
+        Console.WriteLine($"The largest number is {largestNum}.");
     }
+
 }
