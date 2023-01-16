e47cf5ec8a1e1c2da40f35518a75e11893e2b814
jmehlberg96 <meh22002@byui.edu>
2023-01-16 13:58:53 -0700

    Learning Acitvity 2 complete

---
+++ prepare/Learning02/resume.cs
@@ -0,0 +1,31 @@
+using System;
+public class Resume{
+
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void ResumeDetails()
+    {
+    }
+
+    public void DisplayResume()
+
+    {
+    
+        Console.WriteLine($"Name: {_name}");
+
+        Console.WriteLine("Jobs:");
+
+        foreach(Job job in _jobs){
+            job.JobDetails();
+
+            job.ShowJob();     
+
+         }
+   
+    }
+
+
+
+
+}
