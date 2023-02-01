63a01a562cef745b9b9827d223de5d506f958eca
jmehlberg96 <meh22002@byui.edu>
2023-02-01 09:42:51 -0700

    fixed indent and whitespace

--- prove/Develop02/entry.cs
+++ prove/Develop02/entry.cs
@@ -1,23 +1,19 @@
 using System.IO;
 
-public class Entry{
-
-  public string _date;
-  public string _response;
-  public string _prompt;
-
-
-
-
-  public void EntryDetails(){
-    Console.WriteLine($"{_date}; {_prompt}");
-    Console.WriteLine($"{_response}");
-  }
-
-
-  public string GetRandomPrompt(){
-         var random = new Random();
-         var prompts = new List<string>{ 
+public class Entry
+{
+    public string _date;
+    public string _response;
+    public string _prompt;
+    public void EntryDetails()
+    {
+        Console.WriteLine($"{_date}; {_prompt}");
+        Console.WriteLine($"{_response}");
+    }
+    public string GetRandomPrompt()
+    {
+        var random = new Random();
+        var prompts = new List<string>{
             "What was the best part of your day?",
             "How did you see the hand of the Lord in your day today?",
             "What did you learn in your studies today?",
@@ -26,23 +22,21 @@ public class Entry{
             "Who influences you the most?",
             "What is one of your greatest blessings and why?"
             };
-         int index = random.Next(prompts.Count);
-         _prompt = prompts[index];
-         return _prompt;
-   }
-
-
-  public string DisplayDate(){
-    DateTime theCurrentTime = DateTime.Now;
-    string dateText = theCurrentTime.ToShortDateString();
-
+        int index = random.Next(prompts.Count);
+        _prompt = prompts[index];
+        return _prompt;
+    }
+    public string DisplayDate()
+    {
+        DateTime theCurrentTime = DateTime.Now;
+        string dateText = theCurrentTime.ToShortDateString();
         return dateText;
-         }
+    }
 
 
 }
 
-    
+
 
 
 
--- prove/Develop02/journal.cs
+++ prove/Develop02/journal.cs
@@ -1,82 +1,72 @@
 using System.IO;
 
-public class Journal{
-
-    
-  public List<Entry>_entries = new List<Entry>();
-
-    
-
-    public void AddEntry(){
-         Entry entry = new Entry();
-
-         string date = entry.DisplayDate();
-         entry._date = date;
-          
-          string prompt = entry.GetRandomPrompt();
-          Console.WriteLine($"{prompt}");
-        
-          entry._prompt = prompt;
-
-          Console.Write("> ");
-          string response = Console.ReadLine();
-          entry._response = response;
-            
-
-          _entries.Add(entry);
-  }
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
 
 
-    public void DisplayJournal(){
-        foreach(Entry entry in _entries){
-       Console.WriteLine();
-       entry.EntryDetails();
-       Console.WriteLine();
+
+    public void AddEntry()
+    {
+        Entry entry = new Entry();
+        string date = entry.DisplayDate();
+        entry._date = date;
+        string prompt = entry.GetRandomPrompt();
+        Console.WriteLine($"{prompt}");
+        entry._prompt = prompt;
+        Console.Write("> ");
+        string response = Console.ReadLine();
+        entry._response = response;
+        _entries.Add(entry);
     }
+    public void DisplayJournal()
+    {
+        foreach (Entry entry in _entries)
+        {
+            Console.WriteLine();
+            entry.EntryDetails();
+            Console.WriteLine();
+        }
     }
-
-
-
-    public void LoadJournal(){
+    public void LoadJournal()
+    {
         _entries.Clear();
-
-         Console.Write("What is the name of your file? ");
-            string fileName = Console.ReadLine();
+        Console.Write("What is the name of your file? ");
+        string fileName = Console.ReadLine();
         String line;
-        try{
-            using(StreamReader sr = new StreamReader(fileName)){
+        try
+        {
+            using (StreamReader sr = new StreamReader(fileName))
+            {
                 line = sr.ReadLine();
-                while(line != null){
+                while (line != null)
+                {
                     string[] entryLines = line.Split('|');
-                        Entry entry = new Entry();
-
-                        entry._date = entryLines[0];
-                        entry._prompt = entryLines[1];
-                        entry._response = entryLines[2];
-
-                         _entries.Add(entry);
-
-                        Console.WriteLine(line);
-                        line = sr.ReadLine();
-               };
-              
+                    Entry entry = new Entry();
+                    entry._date = entryLines[0];
+                    entry._prompt = entryLines[1];
+                    entry._response = entryLines[2];
+                    _entries.Add(entry);
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                };
             }
         }
-        catch(Exception e)
+        catch (Exception e)
         {
             Console.WriteLine("Exception:" + e.Message);
         }
-
     }
-
-        public void SaveJournal(){
-            Console.Write("What is the name of your file? ");
-            string fileName = Console.ReadLine();
-
-            using(StreamWriter sw = new StreamWriter(fileName)){
-                foreach(Entry line in _entries){
-                    sw.WriteLine($"{line._date} | {line._prompt} | {line._response}" );
-
-            }}
+    public void SaveJournal()
+    {
+        Console.Write("What is the name of your file? ");
+        string fileName = Console.ReadLine();
+        using (StreamWriter sw = new StreamWriter(fileName))
+        {
+            foreach (Entry line in _entries)
+            {
+                sw.WriteLine($"{line._date} | {line._prompt} | {line._response}");
+            }
         }
+    }
 }
