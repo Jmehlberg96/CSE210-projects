9a9337e63593e0248d7385865e511622b348c2fd
jmehlberg96 <meh22002@byui.edu>
2023-02-13 11:43:08 -0700

    Lesson 4 complete

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        Console.WriteLine("");
+        
+        MathAssignment math1 = new MathAssignment("Robert Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+
+        Console.WriteLine("");
+
+        WritingAssignment writing1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+         Console.WriteLine(writing1.GetSummary()); 
+         Console.WriteLine(writing1.GetWritingInfo());  
+        
     }
+
 }
---
+++ prepare/Learning04/writing-assignment.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class WritingAssignment:Assignment{
+    //Here we declare the WritingAssignment attributes
+    private string _title = "";
+
+   
+    public WritingAssignment(string studentName, string topic, string title): base(studentName,topic){
+
+        //Here we set the variables that are specific to the WritingAssignment class
+        _title = title;
+    }
+
+//Here you see that we were able to use the attribute from the Assignment class because we created
+//a getter method for the attribute StudentName. Otherwise the Assignment class would have blocked us 
+//from retrieving that information due to it being private. We could have changed private to protected and 
+// we would not have had to make the GetStudentName() but then the attribute would not have been as secure.
+    public string GetWritingInfo(){
+        string studentName = GetStudentName();
+        return $"{_title} by {studentName}";
+    }
+}
